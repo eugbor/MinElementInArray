863262f0c0af17ce55de4a2b6ded4dbe6b90d9ba
eugbor <eugeneborodin@mail.ru>
2016-12-13 18:59:55 +0300

    commit

--- MinElementInArray/Program.cs
+++ MinElementInArray/Program.cs
@@ -24,30 +24,30 @@ namespace MinElementInArray
             {
                 averageIndex = firstIndex + (lastIndex - firstIndex) / 2; // меняем индекс среднего значения
 
-                if (a[firstIndex] < a[averageIndex])
+                if (a[firstIndex] <= a[averageIndex])
                 {
-                    if (!(a[averageIndex + 1] < a[lastIndex]))
+                    if ((firstIndex > 0 && a[firstIndex] < a[averageIndex + 1]) || firstIndex == averageIndex)
                     {
-                        if (averageIndex + 1 == lastIndex)
-                        {
-                            searchValue = a[lastIndex];
-                            break;
-                        }
-                        firstIndex = averageIndex + 1;
-                    }
-                    else
-                    {
-                        searchValue = a[averageIndex + 1];
+                        searchValue = a[firstIndex];
                         break;
                     }
+
+                    firstIndex = averageIndex + 1;
                 }
                 else
                 {
-                    if (averageIndex + 1 == lastIndex)
+                    if ( averageIndex + 1 == lastIndex)
                     {
+                        if (a[averageIndex] < a[lastIndex])
+                        {
+                            searchValue = a[averageIndex];
+                            break;
+                        }
+
                         searchValue = a[lastIndex];
                         break;
                     }
+
                     lastIndex = averageIndex;
                 }
             }
@@ -56,3 +56,35 @@ namespace MinElementInArray
         }
     }
 }
+
+//while(true)
+//            {
+//                averageIndex = firstIndex + (lastIndex - firstIndex) / 2; // меняем индекс среднего значения
+
+//                if (a[firstIndex] < a[averageIndex])
+//                {
+//                    if (!(a[averageIndex + 1] < a[lastIndex]))
+//                    {
+//                        if (averageIndex + 1 == lastIndex)
+//                        {
+//                            searchValue = a[lastIndex];
+//                            break;
+//                        }
+//                        firstIndex = averageIndex + 1;
+//                    }
+//                    else
+//                    {
+//                        searchValue = a[averageIndex + 1];
+//                        break;
+//                    }
+//                }
+//                else
+//                {
+//                    if (averageIndex + 1 == lastIndex)
+//                    {
+//                        searchValue = a[lastIndex];
+//                        break;
+//                    }
+//                    lastIndex = averageIndex;
+//                }
+//            }
